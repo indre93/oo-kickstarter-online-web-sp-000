14d5c8d861ebecf51de217dc421f82f14664c6a8
Indre Bakeer <indre.bakeer@gmail.com>
2019-02-10 02:51:55 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,14 @@
+class Backer
+  attr_accessor :backers
+  attr_reader :backed_projects, :name
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    self.backed_projects << project
+  end
+
+end
